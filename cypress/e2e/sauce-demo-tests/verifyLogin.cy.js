7da85b13fe804dee105a36c29f4e684b51cf5e2f
Christian Andre Fajardo <c.andre.fajardo@gmail.com>
2023-06-06 23:35:58 +0900

    Add fetching data from json file

--- cypress/e2e/sauce-demo-tests/verifyLogin.cy.js
+++ cypress/e2e/sauce-demo-tests/verifyLogin.cy.js
@@ -3,12 +3,15 @@ import loginPage from '../../pages/loginPage'
 
 let url = 'https://demoblaze.com'
 
-
-
 it('Login to Demoblaze page', () => {
 
-    cy.log('=== TEST CASE START ===')
-    loginPage.login(url,'test','test')
-    cy.log('=== TEST CASE END ===')
-    
+    // This will fetch test data from json file
+    cy.fixture('login.json').then((login) => {
+
+        cy.log('=== TEST CASE START ===')
+        loginPage.login(url,login.email,login.password)
+        cy.log('=== TEST CASE END ===')
+
+    })
+
 })
